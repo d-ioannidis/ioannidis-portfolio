5deb3d1baa433410e2de5f0eef876d64c6b8b09e
Dimitris Ioannidis <52883244+d-ioannidis@users.noreply.github.com>
2026-08-07 15:56:54 +0300

    Remove output option from next.config.ts
    Removed output option for static blog configuration.

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export', // <-- ΑΥΤΟ ΕΙΝΑΙ ΤΟ ΚΛΕΙΔΙ ΓΙΑ ΤΟ ΣΤΑΤΙΚΟ BLOG
+  
   images: {
     unoptimized: true, // Απαραίτητο για στατικό export στο Cloudflare
   },
